2526b12e749f7a749c1a8dd2bf97fa35f85ff827
postek <postek@postek.local>
2018-06-06 17:13:21 +0200

    clean base

--- src/javascript/classes/Home/index.js
+++ src/javascript/classes/Home/index.js
@@ -1,27 +1,16 @@
 import Carousel from './Carousel.js'
-import Renderer from '../Exp/Common/Renderer.js'
-import Composer from '../Exp/Common/Composer.js'
 
 class Home {
     constructor(options) {
 			Storage.HomeClass = this
 
-			this.carousel = document.querySelector('.home__carousel')
-      this.canvasContainer = this.carousel.querySelector('#container')
-
 			this.init()
     }
 
     init() {
-			this.carousel = new Carousel(this.carousel, { index: 0 })
-      new Renderer({ container: this.canvasContainer })
-
-      // will have to be better initiated
-      setTimeout( () => { new Composer() }, 500)
 		}
 
     bind = () => {
-      this.carousel.bind()
     }
 }
 
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,16 +1,4 @@
-const THREE = require('three')
-const OBJLoader = require('three-obj-loader')
-OBJLoader(THREE)
-const MTLLoader = require('three-mtl-loader')
-
-import raf from 'raf'
-
 import Home from './classes/Home/index.js'
-import Interface from './classes/Interface/index.js'
-import Experience1 from './classes/Exp/Exp-1/index.js'
-import Experience2 from './classes/Exp/Exp-2/index.js'
-import Experience3 from './classes/Exp/Exp-3/index.js'
-import SoundManager from './classes/Exp/Common/SoundManager.js'
 
 window.Storage = {}
 Storage.CameraClasses = {}
@@ -22,15 +10,4 @@ initCanvas()
 
 function initCanvas() {
 	new Home()
-	new Interface()
-	new Experience1()
-	new Experience2()
-	new Experience3()
-	new SoundManager()
-
-	raf.add(render)
-}
-
-function render() {
-	if (Storage.HomeCarouselClass && Storage.RendererClass)	Storage.RendererClass.render()
 }
